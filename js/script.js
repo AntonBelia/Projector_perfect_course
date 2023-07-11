5495cb61422da7737767bfd78203768774e02e34
AntonBelia <134911799+AntonBelia@users.noreply.github.com>
2023-07-11 12:42:34 +0300

    add calculations

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const endDateBtn =  document.querySelector('.date2')
 const forPlayback =  document.getElementById('forPlayback')
 const restartBtn =  document.querySelector('.restart')
 const calculBtn =  document.getElementById('calculate-btn')
+const inputResult =  document.getElementById('result')
+const optionsDays =  document.getElementById('options')
+const measurementsSelect =  document.getElementById('measurements')
 
 document.addEventListener('DOMContentLoaded', checkingLocalStorage)
 startDateBtn.addEventListener('click', setStartDateToLocalStorage)
@@ -45,20 +48,42 @@ function restart() {
 }
 
 
-function calcul(dateStart = localStorage.getItem('startDate'), dateEnd = localStorage.getItem('endDate'), dimension) {
-// console.log(dateStart)
-// 	let subtraction = (new Date(dateEnd).getTime() - new Date(dateStart).getTime()) / 1000;
-// 	switch (dimension) {
-// 		case 'seconds':
-// 			return Math.abs(subtraction) + " seconds";
-// 		case 'minutes':
-// 			return Math.abs(subtraction / 60) + ' minutes';
-// 		case 'hours':
-// 			return Math.abs(subtraction / 60 / 60) + ' hours';
-// 		case 'days':
-// 			return Math.abs(subtraction / 60 / 60 / 24).toFixed() + ' days';
-// 		default:
-// 			return Math.abs(subtraction)*1000 + " milliseconds";
-// 	}
+function calcul(num1, num2, dimension = 'month', calculDays = "all") {
+	num1 = new Date(localStorage.getItem('startDate')).getTime()
+	num2 = new Date(localStorage.getItem('endDate')).getTime()
+	dimension = measurementsSelect.value
+	calculDays = optionsDays.value
+	const intermediateResult = unitOfMeasurement(num1, num2, dimension).split(' ')
+
+	switch (calculDays) {
+		case 'all':
+			return inputResult.textContent = intermediateResult.join(' ')
+		case 'weekdays':
+			intermediateResult[0] = (intermediateResult[0] * 5 / 7).toFixed()
+			return inputResult.textContent = intermediateResult.join(' ')
+		case 'weekends':
+			intermediateResult[0] = (intermediateResult[0] * 2 / 7).toFixed()
+			return inputResult.textContent = intermediateResult.join(' ')
+	}
 };
+
+function unitOfMeasurement (num1, num2, dimension) {
+	dimension = measurementsSelect.value
+	const subtraction = (num2 - num1) / 1000;
+
+	switch (dimension) {
+		case 'seconds':
+			return Math.abs(subtraction) + " seconds";
+		case 'minutes':
+			return  Math.abs(subtraction / 60) + ' minutes';
+		case 'hours':
+			return  Math.abs(subtraction / 60 / 60) + ' hours';
+		case 'days':
+			return  Math.abs(subtraction / 60 / 60 / 24).toFixed() + ' days';
+		case 'week':
+			return  Math.abs(subtraction / 60 / 60 / 24 / 7).toFixed() + ' week';
+		case 'month':
+			return  Math.abs(subtraction / 60 / 60 / 24 / 30).toFixed() + ' month';
+	}
+}
 
