c360281f5a5c01adc4fd2c2d21c336540be16206
AntonBelia <134911799+AntonBelia@users.noreply.github.com>
2023-07-18 14:25:15 +0300

    name change

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const startDate = document.getElementById("start-date");
 const endDate = document.getElementById("end-date");
-const calculBtn = document.getElementById("calculate-btn");
+const calculateBtn = document.getElementById("calculate-btn");
 const inputResult = document.getElementById("result");
 const optionsDays = document.getElementById("options");
 const measurementsSelect = document.getElementById("measurements");
@@ -13,88 +13,105 @@ const addWeekBtn = document.getElementById("week");
 document.addEventListener("DOMContentLoaded", renderDataFromLocalStorage);
 startDate.addEventListener("change", setStartDate);
 endDate.addEventListener("change", setEndDate);
-calculBtn.addEventListener("click", calcul);
+calculateBtn.addEventListener("click", timeDifferenceCalculation);
 addMonthBtn.addEventListener("click", addMonth);
 addWeekBtn.addEventListener("click", addWeek);
 
-const date = new Date()
-const timeMonth = 2594159720
-const timeWeek = 606959720
+const date = new Date();
+const timeMonth = 2594159720;
+const timeWeek = 606959720;
 
-function removeAttribute () {
-  endDate.removeAttribute('disabled')
-  calculBtn.removeAttribute('disabled')
+function removeAttribute() {
+  endDate.removeAttribute("disabled");
+  calculateBtn.removeAttribute("disabled");
 }
 
-function createNewDate () {
-  return `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}`
+function createNewDate() {
+  return `${date.getFullYear()}-
+  ${("0" + (date.getMonth() + 1)).slice(-2)}-
+  ${("0" + date.getDate()).slice(-2)}`;
 }
 
-function addMonthToNewDate (date) {
-  const datePlusMonth = new Date(date.getTime() + timeMonth)
-  return `${datePlusMonth.getFullYear()}-${("0" + (datePlusMonth.getMonth() + 1)).slice(-2)}-${("0" + datePlusMonth.getDate()).slice(-2)}`
+function addMonthToNewDate(date) {
+  const datePlusMonth = new Date(date.getTime() + timeMonth);
+  return `${datePlusMonth.getFullYear()}-
+  ${("0" +(datePlusMonth.getMonth() + 1)).slice(-2)}-
+  ${("0" + datePlusMonth.getDate()).slice(-2)}`;
 }
 
-function addWeekToNewDate (date) {
-  const datePlusMonth = new Date(date.getTime() + timeWeek)
-  return `${datePlusMonth.getFullYear()}-${("0" + (datePlusMonth.getMonth() + 1)).slice(-2)}-${("0" + datePlusMonth.getDate()).slice(-2)}`
+function addWeekToNewDate(date) {
+  const datePlusMonth = new Date(date.getTime() + timeWeek);
+  return `${datePlusMonth.getFullYear()}-
+  ${("0" +(datePlusMonth.getMonth() + 1)).slice(-2)}-
+  ${("0" + datePlusMonth.getDate()).slice(-2)}`;
 }
 
-function addMonth () {
-  if (startDate.value === '') {
-    startDate.value = createNewDate ()
-    endDate.value = addMonthToNewDate (date)
-    removeAttribute ()
-  } else if (startDate.value !== '') {
-    endDate.value = addMonthToNewDate (new Date(startDate.value))
-    removeAttribute ()
+function addMonth() {
+  if (startDate.value === "") {
+    startDate.value = createNewDate();
+    endDate.value = addMonthToNewDate(date);
+    removeAttribute();
+  } else if (startDate.value !== "") {
+    endDate.value = addMonthToNewDate(new Date(startDate.value));
+    removeAttribute();
   }
 }
 
-function addWeek () {
-  if (startDate.value === '') {
-    startDate.value = createNewDate ()
-    endDate.value = addWeekToNewDate (date)
-    removeAttribute ()
-  } else if (startDate.value !== '') {
-    endDate.value = addWeekToNewDate (new Date(startDate.value))
-    removeAttribute ()
+function addWeek() {
+  if (startDate.value === "") {
+    startDate.value = createNewDate();
+    endDate.value = addWeekToNewDate(date);
+    removeAttribute();
+  } else if (startDate.value !== "") {
+    endDate.value = addWeekToNewDate(new Date(startDate.value));
+    removeAttribute();
   }
-  
 }
 
 function setStartDate() {
-  console.log(new Date(startDate.value).getTime())
-    endDate.setAttribute('min', startDate.value)
-    endDate.removeAttribute('disabled')
-  }
+  console.log(new Date(startDate.value).getTime());
+  endDate.setAttribute("min", startDate.value);
+  endDate.removeAttribute("disabled");
+}
 
 function setEndDate() {
-    startDate.setAttribute('max', endDate.value);
-    calculBtn.removeAttribute('disabled')
-  }
+  startDate.setAttribute("max", endDate.value);
+  calculateBtn.removeAttribute("disabled");
+}
 
-function calcul(num1, num2, dimension = "days", calculDays = "all") {
+function timeDifferenceCalculation(num1, num2, dimension = "days", calculDays = "all") {
   num1 = new Date(startDate.value).getTime();
   num2 = new Date(endDate.value).getTime();
   dimension = measurementsSelect.value;
   calculDays = optionsDays.value;
   const intermediateResult = unitOfMeasurement(num1, num2, dimension).split(" ");
-  
+
   switch (calculDays) {
     case "all":
-      inputResult.textContent = intermediateResult.join(" ")
-      savingResultsToLocalStorage(startDate.value, endDate.value, inputResult.textContent);
+      inputResult.textContent = intermediateResult.join(" ");
+      savingResultsToLocalStorage(
+        startDate.value,
+        endDate.value,
+        inputResult.textContent
+      );
       return inputResult.textContent;
     case "weekdays":
       intermediateResult[0] = ((intermediateResult[0] * 5) / 7).toFixed();
       inputResult.textContent = intermediateResult.join(" ");
-      savingResultsToLocalStorage(startDate.value, endDate.value, inputResult.textContent);
+      savingResultsToLocalStorage(
+        startDate.value,
+        endDate.value,
+        inputResult.textContent
+      );
       return inputResult.textContent;
     case "weekends":
       intermediateResult[0] = ((intermediateResult[0] * 2) / 7).toFixed();
       inputResult.textContent = intermediateResult.join(" ");
-      savingResultsToLocalStorage(startDate.value, endDate.value, inputResult.textContent);
+      savingResultsToLocalStorage(
+        startDate.value,
+        endDate.value,
+        inputResult.textContent
+      );
       return inputResult.textContent;
   }
 }
@@ -142,11 +159,11 @@ function renderDataFromLocalStorage() {
   history.innerHTML = "";
   const outputData = JSON.parse(localStorage.getItem("savedData")).reverse();
   outputData.forEach((DataElement) => {
-    createOutpudData(DataElement);
+    createOutputData(DataElement);
   });
 }
 
-function createOutpudData(DataElement) {
+function createOutputData(DataElement) {
   const tr = document.createElement("tr");
   tr.innerHTML = `
 	<th>${DataElement[0]}</th>
